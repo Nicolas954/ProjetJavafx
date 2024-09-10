64897c96be53fcf1ef91820615324c09a5072afd
Nicolas954 <n.guillon@lprs.fr>
2024-09-10 23:49:44 +0200

    Ajout de la connexion,inscription et mot de passe oublié au Controller

---
+++ src/main/java/appli/todolistjx/accueil/LoginController.java
@@ -0,0 +1,56 @@
+package appli.todolistjx.accueil;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Button;
+
+
+public class LoginController {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField mdpField;
+
+    @FXML
+    private Label erreurLabel;
+
+    @FXML
+    private Button connexionButton;
+
+    @FXML
+    private Button motDePasseOublieButton;
+
+    @FXML
+    private Button inscriptionButton;
+
+
+    @FXML
+    protected void onConnexionButtonClick() {
+        String email = emailField.getText();
+        String password = mdpField.getText();
+
+        System.out.println("Email: " + email);
+        System.out.println("Mot de passe: " + password);
+
+        if (email==null || email.length()==0 || password==null || password.length()==0) {
+        erreurLabel.setText("Erreur de connexion!");
+        } else {
+            System.out.println("Connexion en cours...");
+        }
+    }
+
+    @FXML
+    private void onMotdepasseoublieButtonClick() {
+        System.out.println("Action : Mot de passe oublié.");
+    }
+
+    @FXML
+    private void onIncriptionButtonClick() {
+        System.out.println("Action : Inscription.");
+    }
+
+}

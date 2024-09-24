b56ccbb540007518df704a4b950f98576f12ab28
GUILLON <n.guillon@lprs.fr>
2024-09-24 15:15:41 +0200

    Ajout de Database

---
+++ src/main/java/database/Database.java
@@ -0,0 +1,11 @@
+package database;
+
+public class Database {
+    private String serveur="localhost";
+    private String nomDeLaBase="bdd";
+    private String Utilisateur="admin";
+    private String MotDePasse="1234";
+
+
+
+}

7b3c6e7e7ddb9309f1e9eddb4956fb93d5fb554b
Nicolas954 <n.guillon@lprs.fr>
2024-10-13 21:50:58 +0200

    Ajout de Initializable

--- src/main/java/appli/todolistjx/accueil/AccueilController.java
+++ src/main/java/appli/todolistjx/accueil/AccueilController.java
@@ -1,11 +1,14 @@
 package appli.todolistjx.accueil;
 import appli.todolistjx.StartApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class AccueilController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class AccueilController implements Initializable {
         @FXML
         private Label welcomeLabel;
 
@@ -21,4 +24,9 @@ public class AccueilController {
             Stage stage = (Stage) welcomeLabel.getScene().getWindow();
             StartApplication.changeScene("loginView.fxml");
         }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
+}
